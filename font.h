6833ef8402c1f46cb7f3d52a3ac7c6138608af47
Renaud Lebrun <renaud_lebrun@yahoo.fr>
2016-11-10 12:47:35 +0100

    Remove EVERYTHING

--- font.h
+++
@@ -1,9 +0,0 @@
-//#if !defined(FONT_H_INCLUDED)
-//#define FONT_H_INCLUDED
-
-void makeRasterFont(void);
-void init(void);
-void printString(char *s);
-//#endif // !defined(FONT_H_INCLUDED)
-void Move2dText(int x, int y);
-//void glText2d(int x, int y, const char *chaine, unsigned baseList ...);
